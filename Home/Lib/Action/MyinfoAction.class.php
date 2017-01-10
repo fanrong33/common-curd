4744b0ec36871749f534e40ed9fea7323200963b
fanrong33 <fanrong33@qq.com>
2017-01-10 17:41:20 +0800

    [improve] 优化修改密码功能

--- Home/Lib/Action/MyinfoAction.class.php
+++ Home/Lib/Action/MyinfoAction.class.php
@@ -53,44 +53,50 @@ class MyinfoAction extends Action{
     }
 
 
+    /** 
+     * 修改密码
+     */
     public function edit_password(){
-        $users = D('Users');
+        $model = D('Users');
 
         if ($this->isPost()) {
 
-            $_POST['password'] = trim($this->_post('password'));
-            
-             
-       
-            if(md5($_POST['password']) != $_SESSION['user']['password']){
-                $this->error('原密码错误');
-            }else{
+            $_validate = array(
+                array('current_password', 'require'  , '当前密码为空', 1),
+                array('new_password'    , 'require'  , '新的密码为空', 1),
+                array('confirm_password', 'require'  , '确认密码不能为空', 1),
+                array('new_password'    , '6,20'     , '密码必须6-20位字符串', 1, 'length'), // in  , between,  length,  等...
+                array('confirm_password', 'new_password', '确认密码与新的密码不一致', 1, 'confirm'),
+            );
 
-                $_POST['new_password'] = trim($this->_post('new_password'));
-                $_POST['confirm_password'] = trim($this->_post('confirm_password'));
-
-                $_validate = array(
-                    array('new_password' , 'require' , '新密码为空' , 1),
-                    array('confirm_password', 'new_password', '确认密码不正确', 1, 'confirm'),
-                );
-
-                  if(false === $data = $users->validate($_validate)->create()){
-                    $this->error($users->getError());
-                }
-                var_dump($data);
-                $cond['password'] = $data['new_password'];
-                var_dump($cond);die;
-             
-                    if ($users->where($cond)->save()) {
-                       $this->ajaxReturn(array('id'=>$data['id']), '修改密码成功', 1);
-                    }else{
-                        $this->error('修改密码失败');
-                    }
-                }
-        
+            $model->setProperty('autoCheckFields', false);
+            if(false === $data = $model->validate($_validate)->create()){
+                $this->error($model->getError());
             }
-        $this->display();
 
+
+            $user = $model->find($this->$_SESSION['user']['id']);
+            if(md5($data['current_password']) != $user['password']){
+                $this->error('当前密码错误');
+            }
+
+            $hash_password = md5($data['new_password']);
+
+
+            $data = array();
+            $data['password']   = $hash_password;
+            $data['updateTime'] = time();
+            $effect = $model->where(array('id'=>$this->$_SESSION['user']['id']))->save($data);
+            
+            if($effect){
+                $this->success('修改成功');
+            }else{
+                $this->error('修改失败');
+            }
+        
+        }
+
+        $this->display();
     }
 
 }
