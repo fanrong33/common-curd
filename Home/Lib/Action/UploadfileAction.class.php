797dc3f8f13679c2a23f36ddff7758f5120b89ef
s53xiaoxiao <1393170420@qq.com>
2017-01-13 11:17:40 +0800

    [fix] 文件

--- Home/Lib/Action/UploadfileAction.class.php
+++ Home/Lib/Action/UploadfileAction.class.php
@@ -16,7 +16,7 @@ class UploadfileAction extends Action{
     public function upload(){
         $upload_img=M('Images');
           	if(!empty($_FILES)){
-              //上传单个图像
+                //上传单个图像
                 import('ORG.Net.UploadFile');
 				$upload                    = new UploadFile();// 实例化上传类
                 $upload->maxSize           = 1*1024*1024 ;// 设置附件上传大小
@@ -28,24 +28,26 @@ class UploadfileAction extends Action{
 			    $upload->thumbMaxWidth     = '50,200';
 			    $upload->thumbMaxHeight    = '50,200';
 			    $upload->thumbPrefix       = 'm_,s_';			    
-				$upload->thumbRemoveOrigin = true;//设置生成缩略图后移除原图
+
                 // 上传单个图片
                 $info   =   $upload->uploadOne($_FILES['image']);       
-                if(!$info) {// 上传错误提示错误信息
+                if(!$info) {
+                	// 上传错误提示错误信息
                     $this->error($upload->getErrorMsg());
-                }else{// 上传成功 获取上传文件信息
-                     $img_url=$info[0]['savepath'].$info[0]['savename'];
-                     $data['img_url']=$img_url;
-                     $data['img_name']=$info[0]['savename'];
-                     $data['create_time']=NOW_TIME;
-                     // var_dump($data);die;
-                     $upload_img->create($data);
-                     $result=$upload_img->add();
-                     if(!$result){
-                         $this->error('上传失败！');
-                     }else{
-                         $this->ajaxReturn('','上传成功',1);
-                     }
+                }else{
+                	// 上传成功 获取上传文件信息
+                    $img_url=$info[0]['savepath'].$info[0]['savename'];
+                    $data['img_url']=$img_url;
+                    $data['img_name']=$info[0]['savename'];
+                    $data['create_time']=NOW_TIME;
+                    // var_dump($data);die;
+                    $upload_img->create($data);
+                    $result=$upload_img->add();
+                    if(!$result){
+                        $this->error('上传失败！');
+                    }else{
+                        $this->ajaxReturn('','上传成功',1);
+                    }
                 }
             }
     }
